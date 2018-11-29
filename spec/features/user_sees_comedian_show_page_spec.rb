d286a3213746062f0746ea93dcf8e7e6d260172a
Daniel Briechle <danbriechle@hotmail.com>
2018-11-28 19:26:25 -0700

    creates test for feature comedians display specials

--- spec/features/user_sees_comedian_show_page_spec.rb
+++ spec/features/user_sees_comedian_show_page_spec.rb
@@ -6,23 +6,36 @@ RSpec.describe 'Comedian Show Page' do
 
       visit '/comedians'
 
-      save_and_open_page
       within "#comic-#{ben.id}" do
         expect(page).to have_content(ben.name)
         expect(page).to have_content("age - #{ben.age}")
         expect(page).to have_content(ben.city)
       end
-      within "#comic-#{dave.id}" do 
+      within "#comic-#{dave.id}" do
         expect(page).to have_content(dave.name)
         expect(page).to have_content("age - #{dave.age}")
         expect(page).to have_content(dave.city)
       end
     end
 
-    # it 'shows comedian list with specials names' do
-    #   ben = Comedian.create(name: 'ben', age: 27, city: "denver")
-    #   dave = Comedian.create(name: 'dave', age: 40, city: "new york")
-    #
-    #   visit '/comedians'
+    it 'shows comedian list with specials names' do
+     daves_special = Special.create(name: "daves really funny")
+     dave = Comedian.create(name: 'dave', age: 40, city: "new york")
+     daves_special.comedians << Comedian.find_by(name: "dave")
+
+     bens_special = Special.create(name: "bens really funny")
+     dave = Comedian.create(name: 'ben', age: 27, city: "denver")
+     bens_special.comedians << Comedian.find_by(name: "ben")
+
+      visit '/comedians'
+      
+      within "#comic-#{ben.id}" do
+        expect(page).to have_content(ben.specials)
+      end
+
+      within "#comic-#{dave.id}" do
+        expect(page).to have_content(dave.specials)
+      end
+    end
   end
 end
